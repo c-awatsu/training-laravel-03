24efed6851ee22ad307eb4e6e718be9d9c14582f
kozooon <kozooonkt@gmail.com>
2019-04-23 16:30:46 +0900

    画面を少々修正

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,15 +3,6 @@
 @section('content')
     {{ Form::open(['route' => 'categories.store']) }}
     @include('components.alert', ['errors' => $errors])
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
     <div class="form-group">
         {{ Form::label('name', 'カテゴリー名：') }}
         {{ Form::text('name', null, ['class' => 'form-control']) }}
